bbf5b72abd03ff1b0be7b6123cec81d75a5fe60b
FarHan949 <uddinfarhan949@gmail.com>
2024-09-21 13:19:21 +0600

    added chapter-02

---
+++ Chapter_02_dataTypes/DataTypes.py
@@ -0,0 +1,20 @@
+# here a 5 types dataTypes in python 
+
+a = 1  # a is an integer type
+
+
+b = 2.55   # b is a floating point  number type
+
+
+c = "Farhan"  # c  is a string type
+
+
+d = True   # d is a boolean type
+
+
+e = None  #  e is a none  type
+
+
+
+
+
---
+++ Chapter_02_dataTypes/varibles.py
@@ -0,0 +1,9 @@
+x = 1
+y = 2
+
+
+c = 7
+
+name = "Farhan"
+
+print(x+y)
