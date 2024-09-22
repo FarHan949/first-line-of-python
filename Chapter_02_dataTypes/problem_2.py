e3410f644792d41452c47d83a6b3c6ed362d57a7
FarHan949 <uddinfarhan949@gmail.com>
2024-09-22 16:30:01 +0600

    added chapter-02 practice set

---
+++ Chapter_02_dataTypes/problem_2.py
@@ -0,0 +1,31 @@
+#1.  write python program to add two numbers 
+a = 69
+b = 31
+# print(a+b)
+
+#2.  write python program to find remainder when number is divided by z 
+
+a1 = 55
+b1 = 105
+# print(a1%b1)
+
+#3.  Check the type of variable assigned USING INPUT () function in python
+
+# x = input("enter the number : ")
+# print(type(x))
+
+
+#4.  Use compression operator to find out whether a given variable is greater than b or not 
+# Take r = 34 , j = 80
+
+# r = int(input("Enter the number 1 : "))
+# j = int(input("Enter the number 2 : "))
+# print("r is greater then j is :" ,  r>j)
+
+
+
+# 5.  write python program to find out an average of tow numbers entered by the user
+
+p = int(input("Please Enter the number  1:"))
+q = int(input("Please Enter the number  2:"))
+print("Average of your Entered  number is :",(p+q)/2)
