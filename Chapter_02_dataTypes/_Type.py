7bc6dd9832aef6d4d9a2ed85a304a85fbf9659b2
FarHan949 <uddinfarhan949@gmail.com>
2024-09-22 15:45:45 +0600

    added chapter-02 type, input function

---
+++ Chapter_02_dataTypes/_Type.py
@@ -0,0 +1,15 @@
+a = 10 
+b = 2.5
+bb = "2.5"
+c = 'Farhan'
+d = True
+e = None
+
+# converting  data types
+print(type(float(bb)))
+print(int(b))
+print(type(str(a)))
+
+
+t =  type(e)
+# print(t)  # Output: <class 'int'>
---
+++ Chapter_02_dataTypes/_input.py
@@ -0,0 +1,7 @@
+a = int(input("Enter  the first number: "))
+b = int(input("Enter  the second number: "))
+
+
+# print("The number a is : ",a)
+# print("The number b is : ",b)
+print("The sum of a and b is : ",a+b)
