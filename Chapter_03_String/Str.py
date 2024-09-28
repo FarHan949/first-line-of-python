3bb42eb2e7d27f1d9d0397424ab44e0bebd4b230
FarHan949 <uddinfarhan949@gmail.com>
2024-09-29 05:23:52 +0600

    added chapter-03 string slicing

--- Chapter_03_String/Str.py
+++ Chapter_03_String/Str.py
@@ -1,11 +1,20 @@
 x = '55'
 y = "Farhan"
 z = '''Kabir
-        Zulfika'''
+ Zulfika'''
 
 S = """
 We got the some of new thing
 """
 
 
-print(z)
+# print(z)
+print(z[0:5])   # start from index 0 all way till 5 excluding 5)
+
+
+# print('''hello world'''.find('''rl'''))
+# print('orange'.count('a'))
+
+
+
+print(len('farhan'))
---
+++ Chapter_03_String/negative_slicing.py
@@ -0,0 +1,6 @@
+name = 'farhan'
+
+print(name[-5 : -1])
+print(name[1 : 5])
+print(name[1: ])   # is same as print(name[1 : 6]) 
+print(name[ : 6])  # is same as print(name[0 : 6]) 
