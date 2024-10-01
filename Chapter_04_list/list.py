4108a75c54fb02f977f7fefd35cce7701b7b0be7
FarHan949 <uddinfarhan949@gmail.com>
2024-10-01 15:39:22 +0600

    added chapter-04 list and list methods

---
+++ Chapter_04_list/list.py
@@ -0,0 +1,10 @@
+fruits = ["apple",  "banana", "cherry", 7, 8, True]
+
+
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+
+print(friends[0])
+friends[0] = "Grapes" # Unlike Strings lists are mutable
+
+print(friends[0])
+print(friends[1:4])
